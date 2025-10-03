924e56ca90c4452b02155b74b0e14e059bafd341
yuo_ota <yuuta.tsunashima@gmail.com>
2025-10-04 01:25:25 +0900

    add : springbootのセットアップを行いました。

---
+++ backend/src/test/java/jp/ac/dendai/spp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.dendai.spp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
