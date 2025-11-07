a8e7e5eeb48e819a9c941d4e305cb60efddac377
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-08 00:46:16 +0900

    refactor : 共通な関数を別のマイグレーションファイルに配置しました。

---
+++ backend/src/main/resources/db/migration/V002__20251107__create__functions.sql
@@ -0,0 +1,8 @@
+-- 更新日時の自動更新トリガー
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
--- backend/src/main/resources/db/migration/V003__20251107__create__users.sql
+++ backend/src/main/resources/db/migration/V003__20251107__create__users.sql
@@ -13,14 +13,5 @@ CREATE TABLE IF NOT EXISTS users (
 -- インデックス
 CREATE INDEX idx_users_email_address ON users(email_address);
 
--- 更新日時の自動更新トリガー
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
