6fe0eef4f3ccb7bba0cbe899b6392868d036a709
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-09 03:54:23 +0900

    fix : linterとformatterの競合をなくしました。

--- backend/src/main/java/jp/ac/dendai/spp/backend/entity/User.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/entity/User.java
@@ -1,13 +1,12 @@
 package jp.ac.dendai.spp.backend.entity;
 
-import java.time.ZonedDateTime;
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
--- backend/src/main/java/jp/ac/dendai/spp/backend/repository/UserRepository.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package jp.ac.dendai.spp.backend.repository;
 
 import java.util.UUID;
-
+import jp.ac.dendai.spp.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import jp.ac.dendai.spp.backend.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {}
