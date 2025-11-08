32fdc194f5225cc2839c25a6969451712f5ac96d
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-08 11:43:20 +0900

    add : storybookにてsvgrを使えるようにしました。

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
   "stories": [
@@ -14,6 +17,16 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [
+        tsconfigPaths(),
+        svgr({
+          include: '**/*.svg',
+        }),
+      ],
+    });
   },
 };
 export default config;
