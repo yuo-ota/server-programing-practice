f229e27d7cc92ac2c2f50dcdc768f67c19369e1f
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-08 13:09:04 +0900

    feat : postページ内LikeButtonコンポーネントを作成しました。

--- frontend/src/features/posts/components/LikeButton.tsx
+++ frontend/src/features/posts/components/LikeButton.tsx
@@ -1,4 +1,5 @@
-import HeartIcon from "../assets/heart.svg?react";
+import InactiveHeartIcon from '../assets/heart_inactive.svg?react';
+import ActiveHeartIcon from '../assets/heart_active.svg?react';
 
 interface LikeButtonProps {
   isLiked: boolean;
@@ -6,18 +7,19 @@ interface LikeButtonProps {
   className?: string;
 }
 
-const LikeButton = ({ isLiked, onClick, className="" }: LikeButtonProps) => {
+const LikeButton = ({ isLiked, onClick, className = '' }: LikeButtonProps) => {
   return (
     <>
       <button
         onClick={onClick}
-        className={className}
+        className={` ${className} hover:bg-theme-color/(--hover-opacity) active:bg-theme-color/(--active-opacity) flex items-center justify-center rounded-full transition-colors duration-150`}
         type="button"
       >
-        <HeartIcon
-          className="w-30 h-30 stroke-1"
-          style={{ fill: isLiked ? "red" : "none", stroke: isLiked ? "red" : "black" }}
-        />
+        {isLiked ? (
+          <ActiveHeartIcon className="fill-theme-color aspect-square w-3/4" />
+        ) : (
+          <InactiveHeartIcon className="fill-theme-color aspect-square w-3/4" />
+        )}
       </button>
     </>
   );
