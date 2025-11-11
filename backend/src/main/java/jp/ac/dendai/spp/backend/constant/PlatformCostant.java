86616e67bed45c413c6ea5248de011387130cb51
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-11 15:24:30 +0900

    add : platformに関する定数を保持するクラスを作成しました。

--- backend/src/main/java/jp/ac/dendai/spp/backend/constant/PlatformCostant.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/constant/PlatformCostant.java
@@ -3,11 +3,11 @@ package jp.ac.dendai.spp.backend.constant;
 import java.util.Map;
 
 public class PlatformCostant {
-  public static final Map<Integer, String> PLATFORM_MAP = Map.of(
-    0, "X",
-    1, "Instagram",
-    2, "Pixiv",
-    3, "Skeb",
-    4, "Bluesky"
-  );
+  public static final Map<Integer, String> PLATFORM_MAP =
+      Map.of(
+          0, "X",
+          1, "Instagram",
+          2, "Pixiv",
+          3, "Skeb",
+          4, "Bluesky");
 }
