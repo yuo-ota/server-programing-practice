da9749ae3e3e3a016a04fb6589ece6f8ea3d4c09
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-14 01:00:23 +0900

    refactor : insert文の形式を統一しました。

--- backend/src/main/resources/db/dev_seeds/V004__20251107__insert__users.sql
+++ backend/src/main/resources/db/dev_seeds/V004__20251107__insert__users.sql
@@ -1,4 +1,5 @@
 INSERT INTO users (email_address, password) VALUES
 ('user1@example.com', '$2a$08$ava0DsxqbPYB7hUYPkOuFOmfCFNH5R0OcTdtcYKxw2tOPZDM9F286'),
 ('user2@example.com', '$2a$08$rdSiSgyoR4XtXOvmjZGjG.Ld4rAEtVrUWlWD5Q5EuKNDGBbGdu5Y2'),
-('user3@example.com', '$2a$08$IoOY0Bvvz1CGJ.YKbIBYauyYwkBburgepn.S7/3ivhAr4u.zdUZEK');
+('user3@example.com', '$2a$08$IoOY0Bvvz1CGJ.YKbIBYauyYwkBburgepn.S7/3ivhAr4u.zdUZEK'),
+('user4@example.com', '$2y$04$xPlav/8V9C55rVHx9PCr6.iXxEH9Qy4HZDF2kkXF9xwVsixj82Ohi');
--- backend/src/main/resources/db/dev_seeds/V006__20251109__insert__admin.sql
+++ backend/src/main/resources/db/dev_seeds/V006__20251109__insert__admin.sql
@@ -1,7 +1,5 @@
-WITH inserted_user AS (
-  INSERT INTO users (email_address, password)
-  VALUES ('user4@example.com', '$2y$04$xPlav/8V9C55rVHx9PCr6.iXxEH9Qy4HZDF2kkXF9xwVsixj82Ohi')
-  RETURNING user_id
-)
 INSERT INTO admin_users (user_id)
-SELECT user_id FROM inserted_user;
+  SELECT
+    users.user_id AS user_id
+  FROM users
+LIMIT 2;
