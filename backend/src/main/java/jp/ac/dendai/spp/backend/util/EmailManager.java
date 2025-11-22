88a6ab15600dfa17eb77db4b1477992ccf2e12aa
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-23 00:13:49 +0900

    refactor : @Autowiredアノテーションをコンストラクタからの代入へ置き換えました。

--- backend/src/main/java/jp/ac/dendai/spp/backend/util/EmailManager.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/util/EmailManager.java
@@ -1,6 +1,5 @@
 package jp.ac.dendai.spp.backend.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,7 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailManager {
 
-  @Autowired private JavaMailSender mailSender;
+  private final JavaMailSender mailSender;
+
+  public EmailManager(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
 
   public void sendSimpleEmail(String to, String subject, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
