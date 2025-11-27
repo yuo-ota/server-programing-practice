a5150c05338e46e06f41885d77fd851243900c6a
hyt-88 <emma_hyt@yahoo.co.jp>
2025-11-28 03:20:17 +0900

    refactor : 見た目を調整しました

--- frontend/src/components/SimpleButton.tsx
+++ frontend/src/components/SimpleButton.tsx
@@ -10,17 +10,17 @@ const SimpleButton = ({
   className = '',
 }: SimpleButtonProps) => {
   return (
-    <>
+    <div className={`${className}`}>
       <button
         onClick={onClick}
-        className={`${className} bg-background transition-brightness flex min-w-24 items-center justify-center rounded-full border hover:brightness-(--hover-nega-brightness) active:brightness-(--active-nega-brightness)`}
+        className= "bg-background transition-brightness flex w-full items-center justify-center rounded-full border hover:brightness-(--hover-nega-brightness) active:brightness-(--active-nega-brightness)"
         type="button"
       >
         <p className="text-subtitle mx-4 my-1 text-center">
           <span className="text-foreground">{label}</span>
         </p>
       </button>
-    </>
+    </div>
   );
 };
 
