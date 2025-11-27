37a43fb41f83ef2330cc32b6cbd67adbcd592c80
hyt-88 <emma_hyt@yahoo.co.jp>
2025-11-27 17:17:58 +0900

    feature : SimpleButtonコンポーネントを実装しました

---
+++ frontend/src/components/SimpleButton.tsx
@@ -0,0 +1,27 @@
+interface SimpleButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string;
+}
+
+const SimpleButton = ({
+  label,
+  onClick,
+  className = '',
+}: SimpleButtonProps) => {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className={`${className} bg-background transition-brightness flex min-w-24 items-center justify-center rounded-full border shadow-md duration-150 hover:brightness-(--hover-nega-brightness) active:brightness-(--active-nega-brightness)`}
+        type="button"
+      >
+        <p className="text-subtitle mx-4 my-1 text-center">
+          <span className="text-foreground">{label}</span>
+        </p>
+      </button>
+    </>
+  );
+};
+
+export default SimpleButton;
