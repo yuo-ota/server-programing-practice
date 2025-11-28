e5af2e4fa6f69e13bdaef58e19c87f3310bd0ff1
23fi121 <14tomo.sushi@gmail.com>
2025-11-28 16:51:11 +0900

    feature : getメソッドの追加

--- backend/src/main/java/jp/ac/dendai/spp/backend/controller/UserController.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/controller/UserController.java
@@ -5,8 +5,10 @@ import java.util.UUID;
 import jp.ac.dendai.spp.backend.error.AuthenticationFailedException;
 import jp.ac.dendai.spp.backend.error.InvalidParameterException;
 import jp.ac.dendai.spp.backend.form.request.CreateUserRequest;
+import jp.ac.dendai.spp.backend.form.request.ShowUserRequest;
 import jp.ac.dendai.spp.backend.form.request.UpdateUserRequest;
 import jp.ac.dendai.spp.backend.form.response.ErrorResponse;
+import jp.ac.dendai.spp.backend.form.response.UserDataResponse;
 import jp.ac.dendai.spp.backend.service.AuthService;
 import jp.ac.dendai.spp.backend.service.UserService;
 import org.springframework.http.HttpHeaders;
@@ -16,11 +18,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("api/user")
@@ -123,4 +128,38 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
   }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<?> show(@RequestHeader("Authorization") String token,
+      @PathVariable("userId") String displayId) {
+    try {
+      ShowUserRequest request = new ShowUserRequest();
+      request.setUserId(displayId);
+
+      UserDataResponse response = userService.show(token, request);
+
+      return ResponseEntity.ok(response);
+    } catch (AuthenticationFailedException e) {
+      ErrorResponse errorResponse = new ErrorResponse();
+
+      errorResponse.setCode("AUTHENTICATION_FAILED");
+      errorResponse.setMessage("ユーザー認証に失敗しました。");
+
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    } catch (InvalidParameterException e) {
+      ErrorResponse errorResponse = new ErrorResponse();
+
+      errorResponse.setCode("BAD_REQUEST_PARAM");
+      errorResponse.setMessage(e.getMessage());
+
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    } catch (Exception e) {
+      ErrorResponse errorResponse = new ErrorResponse();
+
+      errorResponse.setCode("SERVICE_ERROR");
+      errorResponse.setMessage("サーバー内部で予期せぬエラーが発生しました。");
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+  }
+
 }
