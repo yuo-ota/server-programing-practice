34125a3799d2dda57a5eb02ccb0c758a62e6166d
yuo_ota <yuuta.tsunashima@gmail.com>
2025-11-30 22:05:32 +0900

    feat : reportの登録用APIを作成しました。

--- backend/src/main/java/jp/ac/dendai/spp/backend/form/request/CreateReportRequest.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/form/request/CreateReportRequest.java
@@ -1,11 +1,13 @@
 package jp.ac.dendai.spp.backend.form.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import java.util.UUID;
 
 public class CreateReportRequest {
   private String reporteeUser;
   private UUID reporteePost;
-  private String reportType;
+
+  @NotEmpty private String[] reportType;
   private String detail;
 
   // Getters and Setters
@@ -25,12 +27,12 @@ public class CreateReportRequest {
     this.reporteePost = reporteePost;
   }
 
-  public String getReportType() {
-    return reportType;
+  public String[] getReportType() {
+    return reportType.clone();
   }
 
-  public void setReportType(String reportType) {
-    this.reportType = reportType;
+  public void setReportType(String[] reportType) {
+    this.reportType = reportType.clone();
   }
 
   public String getDetail() {
---
+++ backend/src/main/java/jp/ac/dendai/spp/backend/service/ReportService.java
@@ -0,0 +1,74 @@
+package jp.ac.dendai.spp.backend.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import jp.ac.dendai.spp.backend.constant.ReportConstant;
+import jp.ac.dendai.spp.backend.entity.Report;
+import jp.ac.dendai.spp.backend.error.InvalidParameterException;
+import jp.ac.dendai.spp.backend.form.request.CreateReportRequest;
+import jp.ac.dendai.spp.backend.repository.ReportRepository;
+import jp.ac.dendai.spp.backend.repository.UserSettingRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReportService {
+  private final ReportRepository reportRepository;
+  private final UserSettingRepository userSettingRepository;
+
+  public ReportService(
+      ReportRepository reportRepository, UserSettingRepository userSettingRepository) {
+    this.reportRepository = reportRepository;
+    this.userSettingRepository = userSettingRepository;
+  }
+
+  public void createReport(UUID userId, CreateReportRequest request) {
+    if (request.getReporteeUser() != null) {
+      createUserReport(userId, request);
+    } else if (request.getReporteePost() != null) {
+      createPostReport(userId, request);
+    } else {
+      throw new InvalidParameterException("報告対象が指定されていません。");
+    }
+  }
+
+  @Transactional
+  public void createUserReport(UUID userId, CreateReportRequest request) {
+    UUID reporteeUserId =
+        userSettingRepository.findByDisplayId(request.getReporteeUser()).getUserId();
+    List<Integer> categoryStatusId =
+        mapCategoryStatus(request.getReportType(), ReportConstant.REPORT_USER_CATEGORIES_LIST);
+
+    Report report =
+        new Report(userId, true, reporteeUserId, null, categoryStatusId, request.getDetail());
+
+    reportRepository.save(report);
+  }
+
+  @Transactional
+  public void createPostReport(UUID userId, CreateReportRequest request) {
+    List<Integer> categoryStatusId =
+        mapCategoryStatus(request.getReportType(), ReportConstant.REPORT_POST_CATEGORIES_LIST);
+
+    Report report =
+        new Report(
+            userId, false, null, request.getReporteePost(), categoryStatusId, request.getDetail());
+
+    reportRepository.save(report);
+  }
+
+  public List<Integer> mapCategoryStatus(String[] reportTypes, List<String> categoriesList) {
+    List<Integer> categoryStatusId = new ArrayList<>();
+    for (String type : reportTypes) {
+      int index = categoriesList.indexOf(type);
+      if (index != -1) {
+        categoryStatusId.add(index);
+      } else {
+        throw new InvalidParameterException("無効な報告カテゴリが含まれています: " + type);
+      }
+    }
+    return categoryStatusId;
+  }
+}
