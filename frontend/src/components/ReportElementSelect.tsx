c7028f5b8f7f05e7c24ee663037a7ade329853c5
hyt-88 <emma_hyt@yahoo.co.jp>
2025-11-30 23:05:33 +0900

    fix : 項目内容を個別に設定可能にしました

--- frontend/src/components/ReportElementSelect.tsx
+++ frontend/src/components/ReportElementSelect.tsx
@@ -1,62 +1,18 @@
 import { useState } from 'react';
 import CheckBox from './CheckBox';
-
-interface ReportOption {
-  id: string;
-  name: string;
-  label: string;
-  helperText: string;
-}
-
-const reportOptions: ReportOption[] = [
-  {
-    id: 'visibility-settings',
-    name: 'visibility-settings',
-    label: '不適切な公開制限',
-    helperText: '成人向けの内容を含む投稿を、公開制限を行わずに投稿している',
-  },
-  {
-    id: 'hate',
-    name: 'hate',
-    label: 'ヘイト',
-    helperText:
-      '中傷、人種差別主義者または性差別主義者に対する固定概念、ヘイト行為への言及、ヘイトの象徴とロゴ',
-  },
-  {
-    id: 'harassment',
-    name: 'harassment',
-    label: '攻撃的な行為や嫌がらせ',
-    helperText:
-      '侮辱的発言、望ましくない閲覧注意コンテンツや刺激の強いコンテンツ、特定の人物への嫌がらせや嫌がらせの扇動',
-  },
-  {
-    id: 'violence',
-    name: 'violence',
-    label: '暴力的な発言',
-    helperText:
-      '身体的脅迫、危害の願望、暴力の賛美、暴力の扇動、暗号化された暴力の扇動',
-  },
-  {
-    id: 'privacy',
-    name: 'privacy',
-    label: 'プライバシーの侵害',
-    helperText:
-      '個人情報を共有している、個人情報を共有/公開すると脅迫している、合意のない私的な画像を共有している',
-  },
-  {
-    id: 'spam',
-    name: 'spam',
-    label: 'スパムやなりすまし',
-    helperText:
-      '偽のエンゲージメント、詐欺、偽のアカウント、悪意のあるリンク、他の人になりすましている',
-  },
-];
+import type { ReportOption } from '../interfaces/app/ReportOption';
 
 interface ReportElementSelectProps {
+  title: string;
+  reportOptions: ReportOption[];
   className?: string;
 }
 
-const ReportElementSelect = ({ className = '' }: ReportElementSelectProps) => {
+const ReportElementSelect = ({
+  title,
+  reportOptions,
+  className = '',
+}: ReportElementSelectProps) => {
   const [checkedItems, setCheckedItems] = useState<Record<string, boolean>>({});
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -70,7 +26,7 @@ const ReportElementSelect = ({ className = '' }: ReportElementSelectProps) => {
 
   return (
     <div className={`${className} space-y-4 p-10`}>
-      <h2 className="text-title text-foreground">問題の種類を教えてください</h2>
+      <h2 className="text-title text-foreground">{title}</h2>
 
       <div className="space-y-4">
         {reportOptions.map((option) => (
