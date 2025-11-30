946c7466b9c82c82f5b7828ac4ad3ad3fb6df777
hyt-88 <emma_hyt@yahoo.co.jp>
2025-12-01 02:35:39 +0900

    refactor : サイズ関連の修正

--- frontend/src/components/SelectBox.tsx
+++ frontend/src/components/SelectBox.tsx
@@ -29,11 +29,11 @@ const SelectBox = ({
     <div className={`${className} relative inline-block min-h-11 min-w-20`}>
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className={`bg-background border-foreground transition-brightness flex w-full items-center justify-between border px-3 py-2 duration-150 hover:brightness-(--hover-nega-brightness) active:brightness-(--active-nega-brightness) ${displayStatus === 'unrounded-right' ? 'rounded-l-lg' : 'rounded-lg'}`}
+        className={`bg-background border-foreground transition-brightness flex h-full w-full items-center justify-between border px-3 py-2 duration-150 hover:brightness-(--hover-nega-brightness) active:brightness-(--active-nega-brightness) ${displayStatus === 'unrounded-right' ? 'rounded-l-lg' : 'rounded-lg'}`}
         type="button"
       >
         <span
-          className={`text-subtitle ${selected === label ? 'text-placeholder' : 'text-foreground'}`}
+          className={`text-body truncate ${selected === label ? 'text-placeholder' : 'text-foreground'}`}
         >
           {selected}
         </span>
@@ -45,7 +45,7 @@ const SelectBox = ({
       </button>
 
       {isOpen && (
-        <ul className="bg-background border-foreground absolute left-0 z-10 mt-1 max-h-[calc(2.5rem*7)] w-full overflow-hidden overflow-y-auto rounded-lg border shadow-lg">
+        <ul className="bg-background border-foreground absolute left-0 z-10 mt-1 max-h-[calc(2.5rem*7)] overflow-hidden overflow-y-auto rounded-lg border shadow-lg">
           {options.map((opt) => (
             <li key={opt}>
               <button
