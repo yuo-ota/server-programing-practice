0087b999fef48c4fa6b27ab08f8dc3c7f401e4e4
jun0619 <jmizu0619@gmail.com>
2025-12-02 04:39:15 +0900

    feat : KebabMenuComponentを途中まで作成しました。

---
+++ frontend/src/components/KebabMenu.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import DotsIcon from '../assets/dots.svg?react';
+import IconButton from './IconButton';
+
+interface KebabMenuProps {
+  className?: string;
+}
+
+const KebabMenu = ({}: KebabMenuProps) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
