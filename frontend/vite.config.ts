7d75a38c0f4302315a18aa6ca874f999e8317c1c
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-02 14:16:44 +0900

    refactor : インデント漏れを修正しました。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   resolve: {
     alias: {
-    "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   plugins: [
