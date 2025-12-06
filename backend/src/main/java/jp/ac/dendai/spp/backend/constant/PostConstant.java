7154bc56e10a64d93c33e5706636fc4ee8be93de
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-06 21:40:09 +0900

    refactor : 利用していない定数を削除しました.

--- backend/src/main/java/jp/ac/dendai/spp/backend/constant/PostConstant.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/constant/PostConstant.java
@@ -7,8 +7,6 @@ public class PostConstant {
   public static final LocalTime DATE_CHANGE_TIME = LocalTime.of(6, 0);
 
   public static final int DATE_CHANGE_BUFFER_MINUTES = 20;
-  public static final LocalTime DELIVER_POSTS_ALLOCATION_TIME =
-      DATE_CHANGE_TIME.minusMinutes(DATE_CHANGE_BUFFER_MINUTES);
 
   public static final int MAX_DELIVER_POSTS_PER_USER = 20;
 }
