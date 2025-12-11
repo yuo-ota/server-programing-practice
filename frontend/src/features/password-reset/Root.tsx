76c7ad1ec9707b4bf79a19f9b8e9fcef75caf06a
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-11 22:47:41 +0900

    feat : トップバナーを追加しました。

--- frontend/src/features/password-reset/Root.tsx
+++ frontend/src/features/password-reset/Root.tsx
@@ -1,14 +1,29 @@
 import TopBanner from '@/components/TopBanner';
 import PasswordInputGroup from './components/PasswordInputGroup';
+import IconButton from '@/components/IconButton';
+import SettingIcon from '@/assets/allowLeft.svg?react';
+import { useNavigate } from 'react-router';
 
 export const Root = () => {
+  const navigate = useNavigate();
+
+  const handleReturnButtonClick = () => {
+    navigate(-1);
+  }
+
   return (
     <>
       <div className="flex h-full w-full flex-col items-center">
         <TopBanner
-          displayStatus={'normal'}
-          bannerButoonStatus={'solid'}
-          onClick={() => {}}
+          leftElement={
+            <IconButton
+              onClick={handleReturnButtonClick}
+              ButtonIcon={<SettingIcon className={`h-8 w-8`} />}
+              className="w-10 h-10"
+            />
+          }
+          label="パスワード再設定"
+          className="w-full h-16"
         />
         <div className="flex h-full w-full max-w-[500px] flex-col items-center px-8 py-14">
           <PasswordInputGroup />
