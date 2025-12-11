e0b80fddfd8f38e33a7b4e2e8e3e7b219f943bb6
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-11 23:36:51 +0900

    feat : トークン認証ロジックを作成しました。

--- frontend/src/app/routes/password-reset/Verify.tsx
+++ frontend/src/app/routes/password-reset/Verify.tsx
@@ -1,3 +1,52 @@
+import { authToken } from '@/api/AuthParamToken';
+import LoadingAuth from '@/app/auth/LoadingAuth';
+import { TOKEN_TYPE } from '@/constants/tokenType';
+import { Verify as PasswordResetVerify } from '@/features/password-reset/Verify';
+import { useEffect, useRef, useState } from 'react';
+import { Navigate, useSearchParams } from 'react-router';
+
 export const Verify = () => {
-  return <></>;
+  const didInit = useRef(false);
+  const [checking, setChecking] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get('token');
+
+  useEffect(() => {
+    if (!didInit.current) {
+      verifyToken();
+      didInit.current = true;
+    }
+  }, []);
+
+  const verifyToken = async () => {
+    try {
+      if (!token) {
+        throw new Error('No token provided');
+      }
+      const response = await authToken(token, TOKEN_TYPE.PASSWORD_RESET);
+
+      if (response.status === 204) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    } catch {
+      setAuthenticated(false);
+    } finally {
+      setChecking(false);
+    }
+  };
+
+  if (checking) return <LoadingAuth />;
+
+  if (!authenticated) return <Navigate to="/not-found" replace />;
+
+  return (
+    <>
+      <div className="flex h-dvh w-dvw items-center justify-center">
+        <PasswordResetVerify />
+      </div>
+    </>
+  );
 };
---
+++ frontend/src/features/password-reset/Verify.tsx
@@ -0,0 +1,7 @@
+export const Verify = () => {
+
+  return (
+    <>
+    </>
+  );
+};
