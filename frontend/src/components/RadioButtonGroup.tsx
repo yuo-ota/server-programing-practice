f689b3a4500355b240c0fb284bf021e6a757b5b8
hyt-88 <emma_hyt@yahoo.co.jp>
2025-12-12 16:18:15 +0900

    fix : RadioButtonの修正

--- frontend/src/components/RadioButtonGroup.tsx
+++ frontend/src/components/RadioButtonGroup.tsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
 import RadioButton from './RadioButton';
 
 interface RadioButtonGroupProps {
   groupName: string;
   options: string[];
-  initialValue: string;
-  onChange?: (newValue: string) => void;
+  value: string;
+  disabled?: boolean;
+  onSelect: (newValue: string) => void;
   className?: string;
 }
 
 const RadioButtonGroup = ({
   groupName,
   options,
-  initialValue,
-  onChange,
+  value,
+  disabled = false,
+  onSelect,
   className = '',
 }: RadioButtonGroupProps) => {
-  const [selectedValue, setSelectedValue] = useState(initialValue);
 
   const handleChange = (label: string) => {
-    setSelectedValue(label);
-
-    if (onChange) {
-      onChange(label);
+    if (onSelect) {
+      onSelect(label);
     }
   };
 
@@ -30,7 +28,7 @@ const RadioButtonGroup = ({
     <div className={`${className} flex`}>
       {options.map((label, index) => {
         const uniqueId = `${groupName}-${index}`;
-        const isChecked = selectedValue === label;
+        const isChecked = value === label;
 
         return (
           <RadioButton
@@ -39,7 +37,12 @@ const RadioButtonGroup = ({
             name={groupName}
             label={label}
             checked={isChecked}
-            onChange={() => handleChange(label)}
+            disabled={disabled}
+            onChange={() => {
+              if(!disabled){
+                handleChange(label)
+              }
+            }}
           />
         );
       })}
