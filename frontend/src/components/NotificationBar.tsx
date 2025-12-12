241fb1ddafa9e5ccf9812429d616aaf08181f0a2
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-12 22:43:18 +0900

    refactor : NotificationBarコンポーネントのkeyを一意のものになるように修正しました。

--- frontend/src/components/NotificationBar.tsx
+++ frontend/src/components/NotificationBar.tsx
@@ -12,8 +12,8 @@ export default function NotificationBar() {
         className="animate-fade-in-out fixed top-5 z-20 p-4 text-center text-white transition-opacity duration-300"
         style={{ backgroundColor: `var(${notifications.backgroundColor})` }}
       >
-        {notifications.messages.map((msg, index) => (
-          <div key={`notification-bar-${index}`}>{msg}</div>
+        {notifications.messages.map((msg) => (
+          <div key={`notification-bar-${msg}`}>{msg}</div>
         ))}
       </div>
     </div>
