ee1a9ebdda5886c2a7cd608489320b7857939c9d
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-15 02:00:16 +0900

    fix : インポートの更新が行われていない問題を解決しました。

--- frontend/src/components/NotificationBar.tsx
+++ frontend/src/components/NotificationBar.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import NotificationContext from '@/contexts/notificationContext_1';
+import NotificationContext from '@/contexts/notificationContext';
 
 export default function NotificationBar() {
   const { notifications } = useContext(NotificationContext);
