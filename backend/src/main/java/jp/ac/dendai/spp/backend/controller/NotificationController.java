da38ada173bcaa17244fed2922de9b96d0e85f8d
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-18 03:06:54 +0900

    refactor : formatterを通しました。

--- backend/src/main/java/jp/ac/dendai/spp/backend/controller/NotificationController.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/controller/NotificationController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
