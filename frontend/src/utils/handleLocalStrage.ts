b4876aed0a6e7b882297aff09e883b5dd5805074
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-19 00:29:19 +0900

    fix : handleLocalStrage関数のエラーハンドリングを追加しました。

--- frontend/src/utils/handleLocalStrage.ts
+++ frontend/src/utils/handleLocalStrage.ts
@@ -1,8 +1,12 @@
 export const getUserId = () => {
-  const settingData = localStorage.getItem('settingData');
-  if (settingData) {
-    const parsedData = JSON.parse(settingData);
-    return parsedData.user_id as string;
+  try {
+    const settingData = localStorage.getItem('settingData');
+    if (settingData) {
+      const parsedData = JSON.parse(settingData);
+      return parsedData.user_id as string;
+    }
+    return '';
+  } catch {
+    return '';
   }
-  return '';
 };
