86e950218bdc7a42781409abefcd8e01492a87e6
hyt-88 <emma_hyt@yahoo.co.jp>
2025-12-20 22:09:46 +0900

    refactor : 不要なインポートを削除

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 import path from 'node:path';
