24aca199bc383deafca31a610762279bb29e9256
jun0619 <jmizu0619@gmail.com>
2025-12-22 04:13:51 +0900

    feat : PostApiを作成しました

--- frontend/src/api/PostApi.ts
+++ frontend/src/api/PostApi.ts
@@ -1,8 +1,18 @@
 import { API_URL } from '@/config';
+import type { AxiosResponse } from 'axios';
 import axios from 'axios';
+
+
 
 export const createPost = async (formData: FormData): Promise<void> => {
   await axios.post<void>(`${API_URL}/api/post`, formData, {
     withCredentials: true,
   });
 };
+
+export const getPosts = async (date: string): Promise<AxiosResponse<void>> => {
+  const response = await axios.get<void>(`${API_URL}/api/post?date=${date}`, {
+    withCredentials: true,
+  });
+  return response;
+}
