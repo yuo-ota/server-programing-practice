318074d1aa8f86540295748acecb3c4905a9c967
toriatukai <14tomo.sushi@gmail.com>
2025-12-23 00:48:27 +0900

    refactor : 制限の追加

--- backend/src/main/java/jp/ac/dendai/spp/backend/form/request/CreateUserRequest.java
+++ backend/src/main/java/jp/ac/dendai/spp/backend/form/request/CreateUserRequest.java
@@ -9,9 +9,12 @@ import java.util.List;
 import jp.ac.dendai.spp.backend.dto.SocialAccount;
 
 public class CreateUserRequest {
-  @NotBlank private String token;
+  @NotBlank
+  private String token;
 
-  @NotBlank private String name;
+  @Pattern(regexp = "^(?=.*\\S).+$")
+  @NotBlank
+  private String name;
 
   @Past(message = "誕生日は過去の日付で設定してください。")
   private LocalDate birthday;
