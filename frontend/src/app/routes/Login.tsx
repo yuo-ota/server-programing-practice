3ec3a076c41babaf405a642014935d92ecc6c574
yuo_ota <yuuta.tsunashima@gmail.com>
2025-12-23 01:13:13 +0900

    fix : ログイン済みのユーザーにログインを求めることをなくしました。

--- frontend/src/app/routes/Login.tsx
+++ frontend/src/app/routes/Login.tsx
@@ -1,6 +1,46 @@
+import { API_URL } from '@/config';
 import Root from '@/features/login/Root';
+import { useEffect, useRef, useState } from 'react';
+import LoadingAuth from '../auth/LoadingAuth';
+import { Navigate } from 'react-router-dom';
 
 export const Login = () => {
+  const didInit = useRef(false);
+  const [checking, setChecking] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+      if (!didInit.current) {
+        verifyToken();
+        didInit.current = true;
+      }
+    }, []);
+  
+    const verifyToken = async () => {
+      try {
+        const response = await fetch(`${API_URL}/api/auth`, {
+          method: 'POST',
+          credentials: 'include',
+        });
+  
+        if (response.ok) {
+          setAuthenticated(true);
+          setChecking(false);
+        } else {
+          setAuthenticated(false);
+          setChecking(false);
+        }
+      } catch {
+        setAuthenticated(false);
+      } finally {
+        setChecking(false);
+      }
+    };
+
+  if (checking) return <LoadingAuth />;
+
+  if (authenticated) return <Navigate to="/home/posts" replace />;
+
   return (
     <>
       <div className="flex h-dvh w-dvw items-center justify-center">
